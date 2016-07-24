9aa52831e4e4811ad78f5173222322f0640fbb86
woojin <verecahill@gmail.com>
2016-07-24 13:02:57 -0400

    Pooling updated

--- webclient/attendant/src/attendant-app/script/properties.js
+++ webclient/attendant/src/attendant-app/script/properties.js
@@ -1,4 +1,5 @@
 // properties = {"ip" : "http://192.168.1.170:4567"}
-properties = {"ip" : "http://localhost:4567"}
+// properties = {"ip" : "http://localhost:4567"}
 // properties = {"ip" : "http://172.16.31.98:4567"}
 // properties = {"ip" : "http://128.237.190.60:4567"}
+properties = {"ip" : "http://192.168.1.170:4567"}

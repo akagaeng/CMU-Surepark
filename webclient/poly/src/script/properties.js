6488ee68f80bf1200d36cba8cc0f286a10272a0c
woojin <verecahill@gmail.com>
2016-07-23 20:43:35 -0400

    webclient changed

--- webclient/poly/src/script/properties.js
+++ webclient/poly/src/script/properties.js
@@ -1,8 +1,4 @@
 properties = {"ip" : "http://localhost:4567"}
 // properties = {"ip" : "http://172.16.31.98:4567"}
-<<<<<<< HEAD
 // properties = {"ip" : "http://128.237.134.84:4567"}
 // properties = {"ip" : "http://192.168.1.170:4567"}
-=======
-// properties = {"ip" : "http://128.237.190.60:4567"}
->>>>>>> parent of 02f45aa... Woojin changed
